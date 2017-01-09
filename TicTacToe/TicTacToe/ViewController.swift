3649cc4919421ae8038809d2d61ad1dcca4ebdcf
justkevin <kevinbet6@gmail.com>
2017-01-09 23:11:16 +0300

    Day 8 -  tictactoe

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -12,11 +12,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBAction func ButtonPressed(_ sender: Any) {
+    // 1 is noughts, 2 is crosses
+    
+    var activeGame = true
+    
+    var activePlayer = 1
+    
+    var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0] // 0 - empty, 1 - noughts, 2 - crosses
+    
+    let winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    
+    @IBAction func buttonPressed(_ sender: AnyObject) {
         
-        sender.setImage(UIImage(named: "nought.png") , for: [])
+        let activePosition = sender.tag - 1
         
-        //print("ButtonPressed")
+        if gameState[activePosition] == 0 && activeGame {
+            
+            gameState[activePosition] = activePlayer
+            
+            if activePlayer == 1 {
+                
+                sender.setImage(UIImage(named: "nought.png"), for: [])
+                activePlayer = 2
+                
+                
+            } else {
+                
+                sender.setImage(UIImage(named: "cross.png"), for: [])
+                activePlayer = 1
+                
+            }
+
     }
     override func viewDidLoad() {
         super.viewDidLoad()
